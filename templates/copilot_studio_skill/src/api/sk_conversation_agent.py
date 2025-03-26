e600952b4064662ed6dee72341ee2a9270a6faa6
Riccardo Chiodaroli <ricchi@microsoft.com>
2025-03-26 10:08:42 +0100

    [copilot_skill] fix authn

--- templates/copilot_studio_skill/src/api/sk_conversation_agent.py
+++ templates/copilot_studio_skill/src/api/sk_conversation_agent.py
@@ -1,8 +1,14 @@
 from semantic_kernel.agents import ChatCompletionAgent
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+credential = DefaultAzureCredential()
+token_provider = get_bearer_token_provider(
+    credential, "https://cognitiveservices.azure.com/.default"
+)
 
 agent = ChatCompletionAgent(
-    service=AzureChatCompletion(),
+    service=AzureChatCompletion(ad_token_provider=token_provider),
     name="ChatAgent",
     instructions="You invent jokes to have a fun conversation with the user.",
 )
